d69eb88307cce3b3c389b2c79c8e249330204a5a
dauthikimdung <42465061+dauthikimdung@users.noreply.github.com>
2018-08-29 15:48:40 +0700

    k ê

--- web1/sach.js
+++ web1/sach.js
@@ -27,3 +27,46 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     val = setTimeout(showSlides, 3500); // Change image every 2 seconds
 }
+/* show2*/
+
+function currentSlide1(n) {
+    showSlides1(n);
+}
+function showSlides1(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides1");
+    var dots = document.getElementsByClassName("dot1");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (n > slides.length) {n = 1}
+    if (n < 1) {n = slides.length}
+    slides[n-1].style.display = "block";  
+    dots[n-1].className += " active1";
+    
+}
+/*show3*/
+var slideIndex2=0;
+function currentSlide2(n) {
+    clearTimeout(val);
+    slideIndex2 = n-1;
+    showSlides2();
+}
+
+function showSlides2() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides2");
+    var dots = document.getElementsByClassName("dot2");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active1", "");
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    if (slideIndex2 < 1) {slideIndex2 = slides.length}
+
+    slides[slideIndex2-1].style.display = "block";  
+    dots[slideIndex2-1].className += " active1";
+    val = setTimeout(showSlides2, 3000); // Change image every 2 seconds
+}
